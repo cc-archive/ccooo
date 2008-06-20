052533e6f88aef2d2530b37346ac125767f2da31
mihai <mihai@committer.creativecommons.org>
2008-06-20 18:23:10 +0000

    http://www.oooforum.org/forum/viewtopic.phtml?t=67567&highlight=size100thmm After i tried again the solutions from the above topic, seems the ActualSize really works. Strange enough when i tried it 4-5 days ago it didnt worked. So inserting in Writer works now.

--- src/org/creativecommons/openoffice/program/Writer.java
+++ src/org/creativecommons/openoffice/program/Writer.java
@@ -85,26 +85,24 @@ public class Writer extends OOoProgram {
             // insert the graphic at the cursor position
             docCursor.getText().insertTextContent(docCursor, xImage, false);
             
+            Size size = (Size)xProps.getPropertyValue("ActualSize");
+            if (size.Width != 0) {
+                xProps.setPropertyValue("Width",  size.Width); 
+            }
+            else
+                xProps.setPropertyValue("Width",  img.getSelectedImageWidth());
+            if (size.Height != 0) {
+                xProps.setPropertyValue("Height", size.Height);
+            }
+            else
+                xProps.setPropertyValue("Height",  img.getSelectedImageHeigth());
             
-//            //to change !!!
-//            Size size = (Size)xProps.getPropertyValue("ActualSize");
-//            if (size.Width != 0) {
-//                xProps.setPropertyValue("Width",  size.Width); 
-//            }
-//            else
-//                xProps.setPropertyValue("Width",  img.getSelectedImageWidth()*30);
-//            if (size.Height != 0) {
-//                xProps.setPropertyValue("Height", size.Height);
-//            }
-//            else
-//                xProps.setPropertyValue("Height",  img.getSelectedImageHeigth()*30);
-            
-            com.sun.star.uno.Any xGraphic =  (com.sun.star.uno.Any)xProps.getPropertyValue("Graphic");
-            XPropertySet propGraphic = (XPropertySet) UnoRuntime.queryInterface(XPropertySet.class, xGraphic);
-            Size graphicSize = (Size) propGraphic.getPropertyValue("Size100thMM");
-            Size graphicSize1 = (Size) propGraphic.getPropertyValue("SizePixel");
-             xProps.setPropertyValue("Width",  graphicSize.Width); 
-             xProps.setPropertyValue("Height",  graphicSize.Height); 
+//            com.sun.star.uno.Any xGraphic =  (com.sun.star.uno.Any)xProps.getPropertyValue("Graphic");
+//            XPropertySet propGraphic = (XPropertySet) UnoRuntime.queryInterface(XPropertySet.class, xGraphic);
+//            Size graphicSize = (Size) propGraphic.getPropertyValue("Size100thMM");
+//            Size graphicSize1 = (Size) propGraphic.getPropertyValue("SizePixel");
+//             xProps.setPropertyValue("Width",  graphicSize.Width); 
+//             xProps.setPropertyValue("Height",  graphicSize.Height); 
             
             // remove the helper-entry
             xBitmapContainer.removeByName(sName);
